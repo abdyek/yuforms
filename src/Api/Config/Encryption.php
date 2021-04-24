ca04fa0d0101d3ed13d76a1c0cda77b88f733a1a
Yunus Emre Bulut <yunusemrebulut123@gmail.com>
2021-04-24 17:05:52 +0300

    Config/Encryption and Other/Encryption classes added to encrypt slug

---
+++ src/Api/Config/Encryption.php
@@ -0,0 +1,11 @@
+<?php
+namespace Yuforms\Api\Config;
+
+class Encryption {
+    const SEPARATOR = 't'; // must be different numbers and a,b,c,d,e,f
+    const HASH_ALGO = 'sha256';
+    const CONFUSER_FIRST = "*jY^cYHZa\].(-&&sR*/{9.&5d=dWE'W";
+    const CONFUSER_LAST = "P>gg8_eJ+`C`nF.2F)6FD%e3rAC[),.m";
+    const STARTING_INDEX = 3;
+    const HASH_LEN = 20;
+}
---
+++ src/Api/Other/Encryption.php
@@ -0,0 +1,22 @@
+<?php
+namespace Yuforms\Api\Other;
+use Yuforms\Api\Config\Encryption as EncryptionConfig;
+
+class Encryption {
+    public static function encryptSlug($id) {
+        $hex = dechex($id);
+        $string = EncryptionConfig::CONFUSER_FIRST . $hex . EncryptionConfig::CONFUSER_LAST;
+        $hashedString = hash(EncryptionConfig::HASH_ALGO,$string);
+        $shortHashedString = substr($hashedString, EncryptionConfig::STARTING_INDEX, EncryptionConfig::HASH_LEN);
+        return $hex . EncryptionConfig::SEPARATOR . $shortHashedString;
+    }
+    public static function checkEncryptedSlug($encSlug) {
+        $parts = explode(EncryptionConfig::SEPARATOR, $encSlug, 2);
+        if(count($parts)!==2) {
+            return false;
+        }
+        $id = hexdec($parts[0]);
+        $hash = self::encryptSlug($id);
+        return ($encSlug===$hash);
+    }
+}
